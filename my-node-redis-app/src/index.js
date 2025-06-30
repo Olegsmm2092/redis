c7cc7ed954638337e716a5974ab31995ec32736e
Olegsmm2092 <57890633+Olegsmm2092@users.noreply.github.com>
2025-06-30 06:44:17 +0000

    8.2. Running Multiple Commands at the Same Time

--- my-node-redis-app/src/index.js
+++ my-node-redis-app/src/index.js
@@ -17,3 +17,30 @@ await client.connect();
 await client.set('foo', 'bar');
 const result = await client.get('foo');
 console.log(result);  // >>> bar
+
+const run = async () => {
+    await client.hSet('car1', {
+        color: 'red',
+        model: 'Tesla',
+        year: 2023
+    });
+    await client.hSet('car2', {
+        color: 'blue',
+        model: 'Mustang',
+        year: 2024
+    });
+    await client.hSet('car3', {
+        color: 'white',
+        model: 'Ford',
+        year: 2025
+    });
+
+    const cars = await Promise.all([
+        client.hGetAll('car1'),
+        client.hGetAll('car2'),
+        client.hGetAll('car3')
+    ]);
+    console.log(cars);
+};
+
+run();
